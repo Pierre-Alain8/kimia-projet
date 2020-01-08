9f0172bab58b1c72ccda6ed4fc1bdeced935dbac
Pierre-Alain8 <pierrealainagn@gmail.com>
2020-01-08 16:05:01 +0100

    base projet

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Route, Switch, BrowserRouter as Router} from 'react-router-dom';
+import './App.css';
+import Accueil from './components/accueil'
+
+
+class App extends React.Component {
+  render() {
+      return (
+          <Router>
+              <Switch>
+                  <Route exact path="/" component={Accueil} />
+              </Switch>
+          </Router>
+      );
+  }
+}
+
+export default App;
---
+++ src/components/accueil.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { withRouter} from 'react-router-dom';
+// navbar Ricado
+// header Diane
+// instagram PA
+// contact Diane
+// footer PA
+
+class Accueil extends React.Component {
+
+
+
+    render() {
+        return (
+            <div>
+            <h1> hello</h1>
+            </div>
+        )
+    }
+
+}
+
+
+
+export default withRouter(Accueil);
