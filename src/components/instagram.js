d96cfb50274f2bd61db199f4131a7d8223442496
Pierre-Alain8 <pierrealainagn@gmail.com>
2020-01-08 17:14:20 +0100

    component instagram

---
+++ src/components/instagram.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+
+
+class Instagram extends React.Component {
+
+            
+    render() {
+
+        return (
+        <div className="instagramContainer">
+            <h2>INSTAGRAM</h2>
+
+            <div className="instagramFeed">
+
+                <div> 
+                    <img  />
+                </div>
+
+                <div> 
+                    <img  />
+                </div>
+
+                <div> 
+                    <img  />
+                </div>
+            </div>
+        </div>
+
+        );
+    }
+
+
+}
+
+export default (Instagram);
