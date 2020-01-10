9115e5ecc2cc2ef150509d0aa2602f8a07c0fe9e
M'BOUKOU MONEKENE Aymard Ricardo <aymardmb@gmail.com>
2020-01-10 11:19:24 +0100

    apparition des components de la navbar (au clic) dans l'URL (localhost/component)

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 
 class Navabar extends React.Component {
     render () {
         return(
+            <Router>
         <div className="Navbar">
             <header id="header" role="banner" className="main-header">
                 <nav className="header-nav">
                 <div className="navbar-brand">Kimia</div>
                     <ul>
-                        <li><Link to="/">Cantine</Link></li>
-                        <li><Link to="/">Epicerie</Link></li>
-                        <li><Link to="/">Enfants</Link></li>
-                        <li><Link to="/">Association</Link></li>
-                        <li><Link to="/">Evénements</Link></li>
-                        <li><Link to="/">Equipe</Link></li>
-                        <li><Link to="/">Contact</Link></li>
+                        <li><Link to="/Cantine">Cantine</Link></li>
+                        <li><Link to="/Epicerie">Epicerie</Link></li>
+                        <li><Link to="/Enfants">Enfants</Link></li>
+                        <li><Link to="/Association">Association</Link></li>
+                        <li><Link to="/Evenement">Evénements</Link></li>
+                        <li><Link to="/Equipe">Equipe</Link></li>
+                        <li><Link to="/Contact">Contact</Link></li>
                     </ul>
                 </nav>
             </header>
         </div>
+        </Router>
         )
     }
 }
