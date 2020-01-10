d1bd2f04363daeda1964b265f4702740bc82293f
Diane Mpacko <diane.mpacko@gmail.com>
2020-01-10 19:36:29 +0100

    change files name

--- src/components/accueil.js
+++ src/components/accueil.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { withRouter} from 'react-router-dom';
-import Header from './header';
+import Header from './Header';
 import '../css/accueil.css';
-import Pdpcol from './pdp_col';
+import Pdpcol from './Pdp_col';
 // navbar Ricado
 // instagram PA
 // contact Diane
