22e07ab6bbb90297f02238c7b338ed5852123933
M'BOUKOU MONEKENE Aymard Ricardo <aymardmb@gmail.com>
2020-01-13 16:03:10 +0100

    Ajustement Page d'Accueil après merge

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom';
-import './accueil.css';
+import '../src/';
 import App from './App';
 
 
