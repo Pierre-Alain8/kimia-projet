44a10165962fb9e6e59a07e2515c8e3f51131046
M'BOUKOU MONEKENE Aymard Ricardo <aymardmb@gmail.com>
2020-01-14 14:18:22 +0100

    components/Footer M.A.J.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 export default class Footer extends Component {
+
     render() {
         return (
             <div className="Footer">
-    
                 <p> 2020 © Kimia</p>
             </div>
   
