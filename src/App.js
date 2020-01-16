1012fe369a127433bda3cdec9fce47617e618ff9
Pierre-Alain8 <pierrealainagn@gmail.com>
2020-01-16 18:12:01 +0100

    app.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-// import './App.css';
 import Accueil from './components/Accueil';
 import Navabar from './components/Navbar';
 import Cantine from './components/Cantine';
