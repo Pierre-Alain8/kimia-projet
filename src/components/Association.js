f82c4bbb653df8cf30ec57b15ddb3c2c99428c84
Diane Mpacko <diane.mpacko@gmail.com>
2020-01-16 13:21:12 +0100

    update css

--- src/components/Association.js
+++ src/components/Association.js
@@ -6,33 +6,32 @@ class Association extends React.Component{
     render() {
         return(
             
-            <div className="component-asso" >
-                <div className="asso-content" > 
-                    <section className="asso-img" >
-                        <img alt="solidarité" src={assopic} />              
+            <div className="component center" >
+                <div className="component-conteneur cols-space"> 
+                    <section className="deux-cols-space">
+                        <img alt="solidarité" className="deux-cols-space-img" src={assopic} />              
                     </section>
 
-                    <section className="asso-text" >
+                    <section className="deux-cols-space col-text">
                         
-                        <div></div>
-                            <div className="asso-titre" >
+                            <div className="col-text-section" >
                                 <h1>L'ASSOCIATION</h1>
                             </div>
-                            <div className="asso-pavtext" >
+                            <div className="col-text-pavtext" >
                                 <h2>Remettre l'humain et la nature au centre de nos intérêts</h2>
                                 <p> en magasin sont tous formés aux spécificités des produits biologiques afin de  en magasin sont tous formés aux spécificités des produits biologiques afin de  en magasin sont tous formés aux spécificités des produits biologiques afin de  en magasin sont tous formés aux spécificités des produits biologiques afin de  en magasin sont tous formés aux spécificités des produits biologiques afin de </p>
                             </div>
-                            <div className="asso-col" >
-                                <article className="asso-col-un" >
+                            <div className="cols col-text-tableau" >
+                                <article className="deux-cols center col-text" >
                                     <p>Produits locaux</p>
                                     <p>Produits revalorisés</p>
                                     <p>Cuisine faite maison</p>
                                 </article>
-                                <article className="asso-col-deux" >
+                                <article className="deux-cols center" >
                                     <p>Envie de bien manger dans un cadre design ? Au sein du très branché concept-store Merci, La Cantine sert tous les jours un déjeuner sain et équilibré. La carte qui varie au fil des saisons propose des salades et des produits frais  .</p>
                                 </article>
                             </div>
-                            <div className="onglet-contact" >
+                            <div className="col-text-onglet" >
                                 <p>Découvrir l'équipe</p>
                             </div>
 
