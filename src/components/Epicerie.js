5680d2ac230c75523aa7d418ef618522ec8f3712
M'BOUKOU MONEKENE Aymard Ricardo <aymardmb@gmail.com>
2020-01-17 10:36:26 +0100

    Mise en page du component Epicerie

--- src/components/Epicerie.js
+++ src/components/Epicerie.js
@@ -1,11 +1,77 @@
 import React from 'react';
+import epicerie from '../img/Silos_epicerie.jpg'
+
 
 class Epicerie extends React.Component{
+
     render() {
         return(
-            <div>
-                <h1>Epicerie</h1>
-            </div>
+            <section className="component">
+                   
+                <div className="img-content">
+                    <img src={epicerie} alt="epicerie" />
+                </div>
+                
+                <article className="cart-bloc" >
+                        
+                    <div className="title-bloc" >
+                        
+                        <h1>EPICERIE</h1>
+                        <h2>des bons produits locaux pour le quotidient</h2>
+                        <p>lorem ipsum Ut aliquet porta egestas. Aliquam erat volutpat. Pellentesque euismod ipsum non mi volutpat euismod.lorem ipsum Ut aliquet porta egestas. Pellentesque euismod ipsum non mi volutpat euismod.lorem ipsum Ut aliquet porta egestas.</p>
+                    </div>
+                   
+
+                    <div className="wrapper">
+
+                    <div className="cart-content">
+                    <div className="text-content item">
+                        <h5>confiserie</h5>
+                    </div>
+                    <div className="text-content item">
+                        <h5>fruits secs</h5>
+                    </div>
+                    <div className="text-content item">
+                        <h5>confiserie</h5>
+                    </div>
+                    <div className="text-content item">
+                        <h5>fruits secs</h5>
+                    </div>
+                    <div className="text-content item">
+                        <h5>confiserie</h5>
+                    </div>
+                    <div className="text-content item">
+                        <h5>fruits secs</h5>
+                    </div>
+                    </div>
+                
+                    
+                
+                
+                    <div className="cart-content">
+                    <div className="text-content item">
+                        <h5>confiserie</h5>
+                    </div>
+                    <div className="text-content item">
+                        <h5>fruits secs</h5>
+                    </div>
+                    <div className="text-content item">
+                        <h5>confiserie</h5>
+                    </div>
+                    <div className="text-content item">
+                        <h5>fruits secs</h5>
+                    </div>
+                    <div className="text-content item">
+                        <h5>confiserie</h5>
+                    </div>
+                    <div className="text-content item">
+                        <h5>fruits secs</h5>
+                    </div>
+                    </div>
+                    </div>
+                </article>
+
+            </section>
         )
     }
 }
