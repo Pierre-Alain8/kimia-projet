b709cd1240fa83bf891f8d87a84693524d76a78c
M'BOUKOU MONEKENE Aymard Ricardo <aymardmb@gmail.com>
2020-01-20 10:57:54 +0100

    mise à jour component Epicerie

--- src/components/Evenements.js
+++ src/components/Evenements.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import './evenement.css'
 
 class Evenments extends React.Component{
     render() {
         return(
-            <div>
+            <section className="component-event">
                 <h1>Evenments</h1>
-                <h1>Evenments</h1>
-                <h1>Evenments</h1>
-                <h1>Evenments</h1>
-                <h1>Evenments</h1>
-            </div>
+                    <div className="month-list">
+                        <ul className="list-description">
+                            <li className="month-item">janvier</li>
+                            <li className="month-item">févier</li>
+                            <li className="month-item">mars</li>
+                            <li className="month-item">avril</li>
+                            <li className="month-item">mai</li>
+                            <li className="month-item">juin</li>
+                            <li className="month-item">juillet</li>
+                            <li className="month-item">aout</li>
+                            <li className="month-item">septembre</li>
+                            <li className="month-item">octoble</li>
+                            <li className="month-item">novembre</li>
+                            <li className="month-item">décembre</li>
+                        </ul>
+                    </div>
+            </section>
         )
     }
 }
