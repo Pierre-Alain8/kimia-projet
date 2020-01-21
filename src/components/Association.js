a654849a8773fc0caa1aa981584e31c590ae32db
Diane Mpacko <diane.mpacko@gmail.com>
2020-01-21 15:26:49 +0100

    pre-merge css

--- src/components/Association.js
+++ src/components/Association.js
@@ -7,32 +7,33 @@ class Association extends React.Component{
             
             <div className="component-full-height center" >
                 <div className="component-conteneur cols-space center"> 
+                
                     <section className="deux-cols-space">
                         <img alt="solidarité" className="deux-cols-space-img" src={assopic} />              
                     </section>
 
                     <section className="deux-cols-space col-text height90">
                         
-                            <div className="col-text-section" >
-                                <h1>L'ASSOCIATION</h1>
-                            </div>
-                            <div className="col-text-pavtext col-text" >
-                                <h2>Remettre l'humain et la nature au centre de nos intérêts</h2>
+                        <div className="col-text-section" >
+                            <h1>L'ASSOCIATION</h1>
+                        </div>
+                        <div className="col-text-pavtext col-text" >
+                            <h2>Remettre l'humain et la nature au centre de nos intérêts</h2>
                                 <p> en magasin sont tous formés aux spécificités des produits biologiques afin de  en magasin sont tous formés aux spécificités des produits biologiques afin de  en magasin sont tous formés aux spécificités des produits biologiques afin de  en magasin sont tous formés aux spécificités des produits biologiques afin de  en magasin sont tous formés aux spécificités des produits biologiques afin de </p>
-                            </div>
-                            <div className="cols col-text-tableau" >
-                                <article className="deux-cols center col-text" >
-                                    <p>Produits locaux</p>
-                                    <p>Produits revalorisés</p>
-                                    <p>Cuisine faite maison</p>
-                                </article>
-                                <article className="deux-cols center" >
-                                    <p>Envie de bien manger dans un cadre design ? Au sein du très branché concept-store Merci, La Cantine sert tous les jours un déjeuner sain et équilibré. La carte qui varie au fil des saisons propose des salades et des produits frais  .</p>
-                                </article>
-                            </div>
-                            <div className="col-text-onglet" >
-                                <p>Découvrir l'équipe</p>
-                            </div>
+                        </div>
+                        <div className="cols col-text-tableau" >
+                            <article className="deux-cols center col-text" >
+                                <p>Produits locaux</p>
+                                <p>Produits revalorisés</p>
+                                <p>Cuisine faite maison</p>
+                            </article>
+                            <article className="deux-cols center" >
+                                <p>Envie de bien manger dans un cadre design ? Au sein du très branché concept-store Merci, La Cantine sert tous les jours un déjeuner sain et équilibré. La carte qui varie au fil des saisons propose des salades et des produits frais  .</p>
+                            </article>
+                        </div>
+                        <div className="col-text-onglet" >
+                            <p>Découvrir l'équipe</p>
+                        </div>
 
 
                     </section>
