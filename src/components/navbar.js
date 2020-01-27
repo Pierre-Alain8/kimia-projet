11c3196d7c4b43098c2097c3e5f477baf80cd43a
Pierre-Alain8 <pierrealainagn@gmail.com>
2020-01-27 16:24:38 +0100

    navbar scroll : ok

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,9 +2,6 @@ import React from 'react';
 import classnames from "classnames";
 import { Link } from 'react-router-dom'; 
 
-// var { Router, Route, IndexRoute, Link } = ReactRouter;
-
-
 
 class Navabar extends React.Component {
     
