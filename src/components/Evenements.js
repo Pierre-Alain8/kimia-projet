12c99958bc825895097812746c1d76f1db98debc
M'BOUKOU MONEKENE Aymard Ricardo <aymardmb@gmail.com>
2020-01-30 19:56:42 +0100

    Silder/responsive

--- src/components/Evenements.js
+++ src/components/Evenements.js
@@ -2,33 +2,33 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import SliderEvents from './SliderEvents';
 import './evenement.css'
-import 'slick-carousel/slick/slick.css'; 
+import 'slick-carousel/slick/slick.css';
 import "slick-carousel/slick/slick-theme.css";
 
 
 class Evenments extends React.Component{
-    
-    
+
+
     render() {
         return(
 
             <section className="component center center-evenement">
-                        
+
                 <div className="col-text-section">
-                     <h1>EVENEMENT</h1>
+                     <h1>EVENEMENTS</h1>
                 </div>
-        
-                <SliderEvents /> 
-                        
+
+                <SliderEvents />
+
                 <div className="col-text-onglet-10"  >
                     <p><Link to="/Association">Découvrir l'association</Link></p>
                 </div>
-                
+
             </section>
         )
     }
-    
-    
+
+
 }
 
 export default Evenments
