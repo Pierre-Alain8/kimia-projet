00ca3a0d91975a928d80cd6fc766a875bf471ace
Pierre-Alain8 <pierrealainagn@gmail.com>
2020-06-09 22:22:25 +0200

    update 2 kimia

--- src/components/accueil.js
+++ src/components/accueil.js
@@ -1,31 +1,23 @@
-import React from 'react';
-import { withRouter} from 'react-router-dom';
-import Header from './Header';
-import Actualite from './Actualite';
-import '../index';
-import '../accueil.css';
+import React from "react";
+import { withRouter } from "react-router-dom";
+import Header from "./Header";
+import Actualite from "./Actualite";
+import "../index";
+import "../accueil.css";
 
 class Accueil extends React.Component {
-
-
-
-    render() {
-        return (
-
-            <section className="section-accueil">
-                <div className="intro">
-                    <Header />
-                </div>
-                <div className="contenu-accueil">
-                    <Actualite />
-                </div>
-            </section>               
-
-        )
-    }
-
+  render() {
+    return (
+      <section className="section-accueil">
+        <div className="intro">
+          <Header />
+        </div>
+        <div className="contenu-accueil">
+          <Actualite />
+        </div>
+      </section>
+    );
+  }
 }
-
-
 
 export default withRouter(Accueil);
