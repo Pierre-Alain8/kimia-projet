8092f6111e4e423a6268b884ab431d11ac72ee70
Pierre-Alain8 <pierrealainagn@gmail.com>
2020-06-09 22:29:54 +0200

    update 3 kimia

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom'
-import ReactDOM from 'react-dom';
-import App from './App';
-import './App.css'
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom";
+import App from "../src/App";
+import "./App.css";
 
-
-
-
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
-
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
